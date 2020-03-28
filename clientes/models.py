543a1cf9ba322004b5a18665e5d20432be0e81cf
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-03-28 19:28:04 -0300

    152. Registrando model no Admin

--- clientes/models.py
+++ clientes/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class Cliente(models.Model):
     nome = models.CharField(max_length=70)
     endereco = models.CharField(max_length=200, blank=False, null=False)
+    salario = models.DecimalField(max_digits=10, decimal_places=2)
+    idade = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome
+    
