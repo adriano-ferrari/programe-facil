ab554cde7122a8d1a6f362dd45677be3ae2371ae
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-03-28 20:50:48 -0300

    154. Models OneToOneField

--- clientes/models.py
+++ clientes/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class CPF(models.Model):
+    numero = models.CharField(max_length=11)
+    data_exp = models.DateTimeField(auto_now=False)
+
+    def __str__(self):
+        return self.numero
+
+
 class Cliente(models.Model):
     nome = models.CharField(max_length=70)
     endereco = models.CharField(max_length=200, blank=False, null=False)
     salario = models.DecimalField(max_digits=10, decimal_places=2)
     idade = models.IntegerField()
     email = models.EmailField()
+    cpf = models.OneToOneField(CPF, blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome
@@ -19,3 +28,4 @@ class Telefone(models.Model):
 
     def __str__(self):
         return self.descricao + ' - ' + self.numero
+
