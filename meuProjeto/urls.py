41e828b6ddc69080b9ea35b7dc048743477ad263
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-03-28 15:31:45 -0300

    Criaçao do arquivo meuProjeto/views.py e modificaçao do arquivo meuProjeto/urls.py

--- meuProjeto/urls.py
+++ meuProjeto/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import home, clientes, cliente_detalhe, cliente_por_nome
 
 urlpatterns = [
+    path('', home),
+    path('clientes/', clientes),
+    path('cliente/<int:id>/', cliente_detalhe),
+    path('cliente/<str:nome>/', cliente_por_nome),
     path('admin/', admin.site.urls),
 ]
