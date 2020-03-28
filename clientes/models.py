761e8a715204f8b563f0743c22cad6b5f41a0c95
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-03-28 19:14:23 -0300

    151. Models Parte 1

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=70)
+    endereco = models.CharField(max_length=200, blank=False, null=False)
