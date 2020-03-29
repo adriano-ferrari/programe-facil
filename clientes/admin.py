6277346e67420c0ff3e22e1643369291cba69537
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-03-28 22:03:42 -0300

    158. Templates condicionais

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Empregado, Telefone,CPF, Departamento
 
+
+class EmpregadoAdmin(admin.ModelAdmin):
+    fields = ('nome', 'endereco')
+    list_display = ('id', 'nome', 'endereco', 'email')
+    list_filter = ('departamentos',)
+    search_fields = ('id', 'nome', 'email')
+
+
 # Register your models here.
-admin.site.register(Empregado)
+admin.site.register(Empregado, EmpregadoAdmin)
 admin.site.register(Telefone)
 admin.site.register(CPF)
 admin.site.register(Departamento)
--- meuProjeto/views.py
+++ meuProjeto/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Olá Mundo')
+    minha_variavel = 'Programe fácil'
+    return render(request, 'index.html', {'minha_variavel': minha_variavel})
